75427eeeb942c7a96216fe09947263280091de36
Yash617raj <114059450+Yash617raj@users.noreply.github.com>
2022-12-12 21:53:02 +0530

    Add files via upload

---
+++ Practice18.py
@@ -0,0 +1,31 @@
+# dictonaries intro
+
+# how to create dictonaries
+# step 1
+user = {'name':'Yash','age': 19}
+# print(user)
+# print(type(user))
+user1=dict(name='Yash', age= 19)
+# print(user1)
+# how to acess data from dictonary
+# print(user['name'])
+# print(user['age'])
+# which type of data can dictonary store?
+# anything
+
+user_info={
+    'name':'Yash',
+    'age': 19,
+    'fav_movies': ['Zindagi na milege dobara'],
+    'fav_tunes': ['awakening','fairy tale'],
+
+
+
+
+}
+# print(user_info['fav_tunes'])
+
+# How to add data to empty dictonary
+user_info2= {}
+user_info2['name']='nishant'
+print(user_info2)
---
+++ Practice21.py
@@ -0,0 +1,12 @@
+# update method
+
+user_info={
+    'name':'Yash',
+    'age': 19,
+    'fav_movies': ['Zindagi na milege dobara'],
+    'fav_tunes': ['awakening','fairy tale'],
+}
+
+more_info= {'state' : 'Gujarat', 'hobbies' : ['coding','dancing','guiter']}
+user_info.update({})
+print(user_info)
